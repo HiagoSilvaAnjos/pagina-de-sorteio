ede8e3319114ccf45e3693faf715700316baa8df
Hiago Silva <hiagosilvaanjos7@gmail.com>
2022-02-11 11:50:57 -0300

    Criei a função que adiciona paraticipantes

---
+++ javascript/scripts.js
@@ -0,0 +1,52 @@
+const inputElement = document.querySelector('.input-name-people');
+const buttonElement = document.querySelector('.button-add-participant');
+const elementDivContainer = document.querySelector('.participants-container');
+
+// Validação do input
+const validateInput = () => inputElement.value.trim().length > 0;
+
+
+const isValidIpunt = () => {
+    const valueInput = validateInput();
+
+    if(!valueInput) {
+       return inputElement.classList.add('error');
+    }
+
+    console.log(inputElement.value)
+    
+    // Adicionar div
+
+    const participantsContainer = document.createElement('div');
+    participantsContainer.classList.add('participant-item');
+
+    // Adicionando parágrafo
+    const participantContent = document.createElement('p');
+    participantContent.innerText = inputElement.value;
+
+    // Adicionando iconi de lixeira
+    const deleteIcon = document.createElement('i');
+    deleteIcon.classList.add('bx');
+    deleteIcon.classList.add('bxs-trash-alt');
+
+    // Adicionar tudo na div participantsContainer
+    participantsContainer.appendChild(participantContent);
+    participantsContainer.appendChild(deleteIcon);
+
+    console.log(participantsContainer);
+
+    // Adicionar a div participantsContainer na div pai elementDivContainer
+
+    elementDivContainer.appendChild(participantsContainer);
+}
+
+const removeError = () => {
+    const valueInput = validateInput();
+
+    if(valueInput) {
+        return inputElement.classList.remove('error');
+    }
+}
+
+inputElement.addEventListener('change', () => removeError())
+buttonElement.addEventListener('click', () => isValidIpunt())
